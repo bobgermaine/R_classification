6087746946ef5614c6f7f948f686234e09792eb8
bobgermaine <37391026+bobgermaine@users.noreply.github.com>
2022-10-15 17:34:15 -0400

    Add files via upload

---
+++ TD_Synnex_Team_2_Classification.R
@@ -0,0 +1,12 @@
+#### TD Synnex - Team 2 ####
+
+#### Classification in R ####
+library(dplyr)
+library(caret)
+library(rpart)
+library(rpart.plot)
+library(class)
+library(e1071)
+library(pROC)
+
+#Read in data file
